ed14bf0ecf40f4a6fd1ab667f18c23f92787529a
Sammzie92 <122966796+Sammzie92@users.noreply.github.com>
2023-03-03 20:30:59 +0300

    Attempt at creating landing page

---
+++ first.js
@@ -0,0 +1,9 @@
+    <script>
+        // Define a function that will be called when the page loads
+        function onPageLoad() {
+            console.log("The page has loaded.");
+        }
+
+        // Call the function when the page loads
+        window.onload = onPageLoad;
+    </script>
